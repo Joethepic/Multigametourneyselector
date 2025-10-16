3e39024fa26739cee99fd07f53b368b990f444d5
Joethepic <60947591+Joethepic@users.noreply.github.com>
2025-10-15 21:55:03 -0500

    Add tool

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multigame_Tournament_game_decider
+{
+    internal class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public Game(string name, string genre)
+        {
+            Name = name;
+            Genre = genre;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Multigame_Tournament_game_decider;
+using System.Text.RegularExpressions;
+StreamWriter writer = new StreamWriter("games.txt");
+StreamReader readlist = new StreamReader("list.txt");
+int gamestourney;
+int gamesttl;
+int game = 1;
+string file;
+int curgame;
+Random randomizer = new Random();
+int index;
+Console.WriteLine("Enter the number of games for this tourament");
+gamestourney = Convert.ToInt32(Console.ReadLine());
+file = readlist.ReadToEnd();
+string[] arrgamesttl = file.Split('\n');
+List<Game> games = new List<Game>();
+string[] currentgame = { };
+foreach (string str in arrgamesttl)
+{
+    currentgame = str.Split(",");
+    games.Add(new Game(currentgame[0], currentgame[1]));
+}
+readlist.Close();
+while (game <= gamestourney)
+{
+    curgame = randomizer.Next(0, games.Count - 1);
+    writer.WriteLine(games[curgame].Name);
+    string curgenre = games[curgame].Genre;
+    foreach (Game s in games.ToList())
+    {
+        if (s.Genre == curgenre)
+        {
+            games.Remove(s);
+        }
+        else
+        {
+
+        }
+    }
+    game++;
+}
+writer.Close();
